69366794264c683ed80db830b12e308d093568b9
VitorAndes <vitor.andes.santos04@gmail.com>
2023-10-07 22:18:18 -0400

    Input-card-login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputCardComponent } from './components/input-card/input-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputCardComponent
   ],
   imports: [
     BrowserModule,
