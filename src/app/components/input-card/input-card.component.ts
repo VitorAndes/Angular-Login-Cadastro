76c5d613465e670803302d996e59ab20e80c8001
VitorAndes <vitor.andes.santos04@gmail.com>
2023-10-08 20:57:20 -0400

    Adicionando Imagen ao Input-card

--- src/app/components/input-card/input-card.component.ts
+++ src/app/components/input-card/input-card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-input-card',
   templateUrl: './input-card.component.html',
-  styleUrls: ['./input-card.component.css']
+  styleUrls: ['./input-card.component.css', './input-card.responsive.component.css']
 })
 export class InputCardComponent {
 
